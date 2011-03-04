728e25da16b32984a4b3475e723da179f4743d4e
Sean Dunn <sd9@sanger.ac.uk>
2011-03-04 10:57:14 +0000

    Added project checks for window splitting Roughed out window docking feature

--- Support/lib/display_helpers.rb
+++ Support/lib/display_helpers.rb
@@ -1,6 +1,17 @@
 require 'singleton'
+require "#{ENV['TM_SUPPORT_PATH']}/lib/escape.rb"
+require 'rubygems'
+require 'appscript'
 
 module WindowBuddy
+  
+  def self.check_for_project!
+    abort("Sorry, only available within a project") unless ENV['TM_PROJECT_DIRECTORY']
+    
+    # I hate having to check for this but I haven't found a way to automate this yet...
+    abort("Please Reveal File in Project first - ^⌘R") unless ENV['TM_SELECTED_FILE']
+  end
+  
   module TmBaseWindow
     # Create the width and height attributes and they're
     # half_width, half_height helper methods
@@ -35,17 +46,31 @@ module WindowBuddy
     attr_reader :origin_x, :origin_y
 
     def initialize
-      @dimentions  = `"$TMTOOLS" get windowOriginAndSize`
-      @origin_x    = dimentions[0]
-      @origin_y    = dimentions[1]
-      @width       = dimentions[2]
-      @height      = dimentions[3]
-      @line_number = ENV['TM_LINE_NUMBER']
+      @dimentions   = `"$TMTOOLS" get windowOriginAndSize`
+      @origin_x     = dimentions[0]
+      @origin_y     = dimentions[1]
+      @width        = dimentions[2]
+      @height       = dimentions[3]
+      @line_number  = ENV['TM_LINE_NUMBER']
+    end
+    
+    def dock_into_project!
+      abort("Tab already docked") if ENV['TM_PROJECT_DIRECTORY']
+      tm = Appscript.app(ENV['TM_APP_PATH'])
+      tm.windows.first.close(:saving => :ask)
+      `"#{ENV['TM_SUPPORT_PATH']}/bin/mate" #{e_sh(ENV['TM_FILEPATH'])}`
+    end
+
+    
+    def project_home
+      return ENV['TM_PROJECT_DIRECTORY'] if ENV['TM_PROJECT_DIRECTORY']
+      
     end
 
     def new_window
       if open_file_in_new_window
-        new_window = self.class.new
+        new_window        = self.class.new
+        self.project_directory = ENV['TM_PROJECT_DIRECTORY']
         new_window.move_to_line_number!
         new_window
       else
@@ -54,11 +79,7 @@ module WindowBuddy
     end
 
     def open_file_in_new_window
-      if ENV['TM_SELECTED_FILE']
-        `"$TMTOOLS" do openFileInNewWindow`
-      else
-        $stderr.puts "Please Reveal File in Project first - ^⌘R"
-      end
+      `"$TMTOOLS" do openFileInNewWindow`
     end
 
     def move_to_line_number!
@@ -76,6 +97,16 @@ module WindowBuddy
     def zoom!
       `"$TMTOOLS" do zoomWindow`
     end
+    
+    # This should only get called for non-project windows
+    # otherwise use ENV['TM_PROJECT_DIRECTORY']
+    def project_directory
+      `xattr -p com.macromates.tm_project_directory "#{ENV['TM_FILEPATH']}"`
+    end
+    
+    def project_directory=(tm_project_directory)
+      `xattr -w com.macromates.tm_project_directory "#{tm_project_directory}" "#{ENV['TM_FILEPATH']}"`
+    end    
   end
 
 end
